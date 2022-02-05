b91ba5f8e320bb1b90b949b772b904ceb31f6d8f
oem <qwertytoks071192@gmail.com>
2022-02-05 21:32:08 +0300

    Bankomate

--- Client.py
+++ Client.py
@@ -14,28 +14,28 @@
 
 #
 #
-bankomat = {
-    100: 100,
-    50: 100,
-    20: 100,
-    10: 100,
-    5: 100,
-    2: 100,
-    1: 100
-}
-
-
-print ("Введите сумму: ")
-num = int(input())
-
-def output_money (num):
-    for key in bankomat.keys():
-        a = int(num // key)
-        num -= a*key
-        bankomat[key] -= a
-        if a != 0:
-            print ("Получите номинал", key, "в количестве", a)
-
-output_money(num)
-
-print(bankomat)
+# bankomat = {
+#     100: 100,
+#     50: 100,
+#     20: 100,
+#     10: 100,
+#     5: 100,
+#     2: 100,
+#     1: 100
+# }
+#
+#
+# print ("Введите сумму: ")
+# num = int(input())
+#
+# def output_money (num):
+#     for key in bankomat.keys():
+#         a = int(num // key)
+#         num -= a*key
+#         bankomat[key] -= a
+#         if a != 0:
+#             print ("Получите номинал", key, "в количестве", a)
+#
+# output_money(num)
+#
+# print(bankomat)
