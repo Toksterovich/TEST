c917299d692a595b39f5c9caf74be94e084f271d
oem <qwertytoks071192@gmail.com>
2022-02-05 21:20:50 +0300

    Bankomate

---
+++ Client.py
@@ -0,0 +1,41 @@
+# def enter_pin ():
+#     while True:
+#         print("Enter PIN:")
+#         code = 1234
+#         pin = int(input())
+#         if pin == code:
+#             print("Hello")
+#             break
+#         else:
+#             for i in range(3):
+#                 enter_pin()
+#
+# enter_pin()
+
+#
+#
+bankomat = {
+    100: 100,
+    50: 100,
+    20: 100,
+    10: 100,
+    5: 100,
+    2: 100,
+    1: 100
+}
+
+
+print ("Введите сумму: ")
+num = int(input())
+
+def output_money (num):
+    for key in bankomat.keys():
+        a = int(num // key)
+        num -= a*key
+        bankomat[key] -= a
+        if a != 0:
+            print ("Получите номинал", key, "в количестве", a)
+
+output_money(num)
+
+print(bankomat)
